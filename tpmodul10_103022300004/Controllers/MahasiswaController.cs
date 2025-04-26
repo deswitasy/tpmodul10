773c7aad7de76361fe7047680ebd482f93cd4a23
deswitasy <deswitas06@gmail.com>
2025-04-26 23:02:40 +0700

    menambahkan API mahasiswa

---
+++ tpmodul10_103022300004/Controllers/MahasiswaController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using tpmodul10_103022300004.Models;
+
+namespace tpmodul10_103022300004.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> _mahasiswaList = new List<Mahasiswa>
+        {
+            new Mahasiswa { Nama = "Deswita Syaharani", Nim = "103022300004" },
+            new Mahasiswa { Nama = "Carlos Sainz", Nim = "103022300055" }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Mahasiswa>> Get()
+        {
+            return Ok(_mahasiswaList);
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<Mahasiswa> Get(int index)
+        {
+            if (index < 0 || index >= _mahasiswaList.Count)
+            {
+                return NotFound();
+            }
+            return Ok(_mahasiswaList[index]);
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] Mahasiswa mahasiswaBaru)
+        {
+            _mahasiswaList.Add(mahasiswaBaru);
+            return Ok();
+        }
+
+        [HttpDelete("{index}")]
+        public ActionResult Delete(int index)
+        {
+            if (index < 0 || index >= _mahasiswaList.Count)
+            {
+                return NotFound();
+            }
+            _mahasiswaList.RemoveAt(index);
+            return Ok();
+        }
+    }
+}
---
+++ tpmodul10_103022300004/Models/Mahasiswa.cs
@@ -0,0 +1,8 @@
+﻿namespace tpmodul10_103022300004.Models
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+    }
+}
